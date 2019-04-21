fcaff7553ee028fc4a9d3b277db6d2de45bb1d74
M Usama Alvi <m.usamaalvi@gmail.com>
2019-04-21 23:10:20 +0500

    Topic container

---
+++ CSCMU/src/app/topic-container/topic-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topic-container',
+  templateUrl: './topic-container.component.html',
+  styleUrls: ['./topic-container.component.css']
+})
+export class TopicContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
