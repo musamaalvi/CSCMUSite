32e183eaec6db80a77f9cb86ffd4183ee5988917
M Usama Alvi <m.usamaalvi@gmail.com>
2019-09-07 22:38:14 +0500

    Adding sandbox files

---
+++ CSCMU/src/app/excersis/excersis.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-excersis',
+  templateUrl: './excersis.component.html',
+  styleUrls: ['./excersis.component.css']
+})
+export class ExcersisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
